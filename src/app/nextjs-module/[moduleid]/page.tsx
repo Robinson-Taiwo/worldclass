b1e448db9c86b9c42d1f7074e4caf70ad32527e2
User <user@Taiwos-MacBook-Pro.local>
2025-07-25 14:04:23 +0100

    FIX: fixed bug in details page

--- src/app/nextjs-module/[moduleid]/page.tsx
+++ src/app/nextjs-module/[moduleid]/page.tsx
@@ -1,12 +1,18 @@
 import { nextjsModules } from "@/data/nextjs";
 import React from "react";
 
-const NextjsModuleDetails = ({ params }: { params: { moduleid: string } }) => {
+interface NextjsModuleDetailsProps {
+  params: {
+    moduleid: string;
+  };
+}
+
+const NextjsModuleDetails = ({ params }: NextjsModuleDetailsProps) => {
   const modules = nextjsModules.find((mod) => mod.id === params.moduleid);
 
   return (
-    <div className="w-full">
-      <div>{modules?.title}</div>;
+    <div className="w-full p-6">
+      <h1 className="text-2xl font-bold">{modules?.title || "Module not found"}</h1>
     </div>
   );
 };
